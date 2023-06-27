4624ee7f356d6208e384071ef99c7ff76a753f59
Filippo Di Pietro <filippodp9845@gmail.com>
2023-06-27 18:56:55 +0200

    Added file for compiling and run cuda tests

---
+++ test_cuda.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+OBJECT_DIR=object
+EXE_DIR=exe
+SRC=src
+
+for test_file in src/test/test-Ford-Fulkerson-cuda-*; do
+    file_name=$(basename "$test_file")
+    file_name="${file_name%.*}"
+    echo $file_name
+    nvcc ${OBJECT_DIR}/DirectedEdge.o ${OBJECT_DIR}/Node.o ${OBJECT_DIR}/Graph.o ${SRC}/ford-fulkerson/Cuda.cu ${test_file} -o ${EXE_DIR}/${file_name}
+done
+
+for exe_test_file in exe/test-Ford-Fulkerson-cuda-*; do
+    echo $exe_test_file
+    ./${exe_test_file}
+done
